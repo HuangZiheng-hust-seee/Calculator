c5ae46a399866b79534316a02c277b61d025896d
HuangZiheng-hust-seee <852827150@qq.com>
2022-08-17 19:34:57 +0800

    主界面1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,283 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Num0 = new System.Windows.Forms.Button();
+            this.dot = new System.Windows.Forms.Button();
+            this.Num1 = new System.Windows.Forms.Button();
+            this.Num2 = new System.Windows.Forms.Button();
+            this.Num3 = new System.Windows.Forms.Button();
+            this.times = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.Num6 = new System.Windows.Forms.Button();
+            this.Num5 = new System.Windows.Forms.Button();
+            this.Num4 = new System.Windows.Forms.Button();
+            this.rightbracket = new System.Windows.Forms.Button();
+            this.leftbracket = new System.Windows.Forms.Button();
+            this.Num9 = new System.Windows.Forms.Button();
+            this.Num8 = new System.Windows.Forms.Button();
+            this.Num7 = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Num0
+            // 
+            this.Num0.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Num0.Location = new System.Drawing.Point(1, 818);
+            this.Num0.Name = "Num0";
+            this.Num0.Size = new System.Drawing.Size(110, 130);
+            this.Num0.TabIndex = 1;
+            this.Num0.Text = "0";
+            this.Num0.UseVisualStyleBackColor = true;
+            // 
+            // dot
+            // 
+            this.dot.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dot.Location = new System.Drawing.Point(117, 818);
+            this.dot.Name = "dot";
+            this.dot.Size = new System.Drawing.Size(110, 130);
+            this.dot.TabIndex = 2;
+            this.dot.Text = "·";
+            this.dot.UseVisualStyleBackColor = true;
+            // 
+            // Num1
+            // 
+            this.Num1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Num1.Location = new System.Drawing.Point(1, 682);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(110, 130);
+            this.Num1.TabIndex = 3;
+            this.Num1.Text = "1";
+            this.Num1.UseVisualStyleBackColor = true;
+            // 
+            // Num2
+            // 
+            this.Num2.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Num2.Location = new System.Drawing.Point(117, 682);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(110, 130);
+            this.Num2.TabIndex = 4;
+            this.Num2.Text = "2";
+            this.Num2.UseVisualStyleBackColor = true;
+            // 
+            // Num3
+            // 
+            this.Num3.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Num3.Location = new System.Drawing.Point(233, 682);
+            this.Num3.Name = "Num3";
+            this.Num3.Size = new System.Drawing.Size(110, 130);
+            this.Num3.TabIndex = 5;
+            this.Num3.Text = "3";
+            this.Num3.UseVisualStyleBackColor = true;
+            // 
+            // times
+            // 
+            this.times.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.times.Location = new System.Drawing.Point(349, 682);
+            this.times.Name = "times";
+            this.times.Size = new System.Drawing.Size(110, 130);
+            this.times.TabIndex = 6;
+            this.times.Text = "×";
+            this.times.UseVisualStyleBackColor = true;
+            // 
+            // divide
+            // 
+            this.divide.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.divide.Location = new System.Drawing.Point(465, 682);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(110, 130);
+            this.divide.TabIndex = 7;
+            this.divide.Text = "÷";
+            this.divide.UseVisualStyleBackColor = true;
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.minus.Location = new System.Drawing.Point(465, 546);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(110, 130);
+            this.minus.TabIndex = 12;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            // 
+            // plus
+            // 
+            this.plus.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.plus.Location = new System.Drawing.Point(349, 546);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(110, 130);
+            this.plus.TabIndex = 11;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            // 
+            // Num6
+            // 
+            this.Num6.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Num6.Location = new System.Drawing.Point(233, 546);
+            this.Num6.Name = "Num6";
+            this.Num6.Size = new System.Drawing.Size(110, 130);
+            this.Num6.TabIndex = 10;
+            this.Num6.Text = "6";
+            this.Num6.UseVisualStyleBackColor = true;
+            // 
+            // Num5
+            // 
+            this.Num5.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Num5.Location = new System.Drawing.Point(117, 546);
+            this.Num5.Name = "Num5";
+            this.Num5.Size = new System.Drawing.Size(110, 130);
+            this.Num5.TabIndex = 9;
+            this.Num5.Text = "5";
+            this.Num5.UseVisualStyleBackColor = true;
+            // 
+            // Num4
+            // 
+            this.Num4.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Num4.Location = new System.Drawing.Point(1, 546);
+            this.Num4.Name = "Num4";
+            this.Num4.Size = new System.Drawing.Size(110, 130);
+            this.Num4.TabIndex = 8;
+            this.Num4.Text = "4";
+            this.Num4.UseVisualStyleBackColor = true;
+            // 
+            // rightbracket
+            // 
+            this.rightbracket.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rightbracket.Location = new System.Drawing.Point(465, 410);
+            this.rightbracket.Name = "rightbracket";
+            this.rightbracket.Size = new System.Drawing.Size(110, 130);
+            this.rightbracket.TabIndex = 17;
+            this.rightbracket.Text = ")";
+            this.rightbracket.UseVisualStyleBackColor = true;
+            // 
+            // leftbracket
+            // 
+            this.leftbracket.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.leftbracket.Location = new System.Drawing.Point(349, 410);
+            this.leftbracket.Name = "leftbracket";
+            this.leftbracket.Size = new System.Drawing.Size(110, 130);
+            this.leftbracket.TabIndex = 16;
+            this.leftbracket.Text = "(";
+            this.leftbracket.UseVisualStyleBackColor = true;
+            // 
+            // Num9
+            // 
+            this.Num9.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Num9.Location = new System.Drawing.Point(233, 410);
+            this.Num9.Name = "Num9";
+            this.Num9.Size = new System.Drawing.Size(110, 130);
+            this.Num9.TabIndex = 15;
+            this.Num9.Text = "9";
+            this.Num9.UseVisualStyleBackColor = true;
+            // 
+            // Num8
+            // 
+            this.Num8.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Num8.Location = new System.Drawing.Point(117, 410);
+            this.Num8.Name = "Num8";
+            this.Num8.Size = new System.Drawing.Size(110, 130);
+            this.Num8.TabIndex = 14;
+            this.Num8.Text = "8";
+            this.Num8.UseVisualStyleBackColor = true;
+            // 
+            // Num7
+            // 
+            this.Num7.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Num7.Location = new System.Drawing.Point(1, 410);
+            this.Num7.Name = "Num7";
+            this.Num7.Size = new System.Drawing.Size(110, 130);
+            this.Num7.TabIndex = 13;
+            this.Num7.Text = "7";
+            this.Num7.UseVisualStyleBackColor = true;
+            // 
+            // del
+            // 
+            this.del.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.del.Location = new System.Drawing.Point(233, 818);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(110, 130);
+            this.del.TabIndex = 18;
+            this.del.Text = "DEL";
+            this.del.UseVisualStyleBackColor = true;
+            // 
+            // C
+            // 
+            this.C.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.C.Location = new System.Drawing.Point(349, 818);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(110, 130);
+            this.C.TabIndex = 19;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            // 
+            // equal
+            // 
+            this.equal.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.equal.Location = new System.Drawing.Point(465, 818);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(110, 130);
+            this.equal.TabIndex = 20;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(578, 944);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.rightbracket);
+            this.Controls.Add(this.leftbracket);
+            this.Controls.Add(this.Num9);
+            this.Controls.Add(this.Num8);
+            this.Controls.Add(this.Num7);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.Num6);
+            this.Controls.Add(this.Num5);
+            this.Controls.Add(this.Num4);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.times);
+            this.Controls.Add(this.Num3);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num1);
+            this.Controls.Add(this.dot);
+            this.Controls.Add(this.Num0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Num0;
+        private System.Windows.Forms.Button dot;
+        private System.Windows.Forms.Button Num1;
+        private System.Windows.Forms.Button Num2;
+        private System.Windows.Forms.Button Num3;
+        private System.Windows.Forms.Button times;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button Num6;
+        private System.Windows.Forms.Button Num5;
+        private System.Windows.Forms.Button Num4;
+        private System.Windows.Forms.Button rightbracket;
+        private System.Windows.Forms.Button leftbracket;
+        private System.Windows.Forms.Button Num9;
+        private System.Windows.Forms.Button Num8;
+        private System.Windows.Forms.Button Num7;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button equal;
     }
 }
 
