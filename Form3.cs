e34b438543b8197d2da511b608f7d371b3952356
HuangZiheng-hust-seee <852827150@qq.com>
2022-08-19 17:28:54 +0800

    test1

---
+++ Form3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form4 FormLocation = new Form4();//创建新窗体
+            this.Hide();
+            FormLocation.ShowDialog();
+            Application.ExitThread();//退出当前窗体
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //窗口跳转
+            Form1 FormCalculator = new Form1();//创建新窗体
+            this.Hide();
+            FormCalculator.ShowDialog();
+            Application.ExitThread();//退出当前窗体
+        }
+    }
+}
---
+++ Form4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Globaldata.Passdata = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 InitialForm = new Form3();//创建新窗体
+            this.Hide();
+            InitialForm.ShowDialog();
+            Application.ExitThread();//退出当前窗体
+        }
+    }
+}
